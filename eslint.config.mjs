5c281b50b8898a3b2151e77adbb7fd39566ede51
Ryan Luong <lumintriet@gmail.com>
2025-07-23 17:06:00 +0700

    Basic create-next-app run.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import prettierConfig from "eslint-config-prettier";
+import { globalIgnores } from "eslint/config";
 
 export default tseslint.config(
+  globalIgnores(["dist/*", "eslint.config.mjs"]),
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
   {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-import { intro, outro } from "@clack/prompts";
+#!/usr/bin/env node
 
-intro(`create-sparkle`);
+import { intro, outro, spinner } from "@clack/prompts";
+import { execa } from "execa";
+
+const s = spinner();
+
+intro(`sparkle-1`);
+
+s.start("Creating sparkle...");
+await execa("pnpx", [
+  "create-next-app@latest",
+  "sparkle-1",
+  "--ts",
+  "--tailwind",
+  "--eslint",
+  "--app",
+  "--no-src-dir",
+  "--turbopack",
+  "--import-alias",
+  "@/*",
+  "--skip-install",
+  "--disable-git"
+]);
+s.stop("Sparkle created!");
 
 outro(`You're all set!`);
